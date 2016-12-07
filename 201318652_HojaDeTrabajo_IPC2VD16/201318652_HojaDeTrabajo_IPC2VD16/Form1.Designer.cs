4727e53aabb63c14e91d5967f7d539fd94604f75
G <201318652@ingenieria.usac.edu.gt>
2016-12-06 19:17:44 -0600

    Commit1: IVA Calcula el IVA sobre un precio con IVA incluido. El resultado es el precio sin IVA, como el IVA en si.

---
+++ 201318652_HojaDeTrabajo_IPC2VD16/201318652_HojaDeTrabajo_IPC2VD16/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace _201318652_HojaDeTrabajo_IPC2VD16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IVAButton = new System.Windows.Forms.Button();
+            this.FCuadraticaButton = new System.Windows.Forms.Button();
+            this.DistanciaButton = new System.Windows.Forms.Button();
+            this.HipotenusaButton = new System.Windows.Forms.Button();
+            this.XTB = new System.Windows.Forms.TextBox();
+            this.ZTB = new System.Windows.Forms.TextBox();
+            this.YTB = new System.Windows.Forms.TextBox();
+            this.WTB = new System.Windows.Forms.TextBox();
+            this.InfoLB = new System.Windows.Forms.Label();
+            this.RLB = new System.Windows.Forms.Label();
+            this.ResultadoLB = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // IVAButton
+            // 
+            this.IVAButton.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.IVAButton.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IVAButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.IVAButton.Location = new System.Drawing.Point(38, 129);
+            this.IVAButton.Name = "IVAButton";
+            this.IVAButton.Size = new System.Drawing.Size(113, 30);
+            this.IVAButton.TabIndex = 0;
+            this.IVAButton.Text = "IVA";
+            this.IVAButton.UseVisualStyleBackColor = false;
+            this.IVAButton.Click += new System.EventHandler(this.IVAButton_Click);
+            // 
+            // FCuadraticaButton
+            // 
+            this.FCuadraticaButton.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.FCuadraticaButton.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FCuadraticaButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FCuadraticaButton.Location = new System.Drawing.Point(439, 226);
+            this.FCuadraticaButton.Name = "FCuadraticaButton";
+            this.FCuadraticaButton.Size = new System.Drawing.Size(113, 30);
+            this.FCuadraticaButton.TabIndex = 1;
+            this.FCuadraticaButton.Text = "F. cuadratica";
+            this.FCuadraticaButton.UseVisualStyleBackColor = false;
+            // 
+            // DistanciaButton
+            // 
+            this.DistanciaButton.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.DistanciaButton.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DistanciaButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.DistanciaButton.Location = new System.Drawing.Point(38, 226);
+            this.DistanciaButton.Name = "DistanciaButton";
+            this.DistanciaButton.Size = new System.Drawing.Size(113, 30);
+            this.DistanciaButton.TabIndex = 2;
+            this.DistanciaButton.Text = "Distancia";
+            this.DistanciaButton.UseVisualStyleBackColor = false;
+            // 
+            // HipotenusaButton
+            // 
+            this.HipotenusaButton.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.HipotenusaButton.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HipotenusaButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.HipotenusaButton.Location = new System.Drawing.Point(439, 129);
+            this.HipotenusaButton.Name = "HipotenusaButton";
+            this.HipotenusaButton.Size = new System.Drawing.Size(113, 30);
+            this.HipotenusaButton.TabIndex = 3;
+            this.HipotenusaButton.Text = "Hipotenusa";
+            this.HipotenusaButton.UseVisualStyleBackColor = false;
+            // 
+            // XTB
+            // 
+            this.XTB.BackColor = System.Drawing.SystemColors.InfoText;
+            this.XTB.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XTB.ForeColor = System.Drawing.Color.DarkOrange;
+            this.XTB.Location = new System.Drawing.Point(38, 86);
+            this.XTB.Name = "XTB";
+            this.XTB.Size = new System.Drawing.Size(100, 25);
+            this.XTB.TabIndex = 4;
+            // 
+            // ZTB
+            // 
+            this.ZTB.BackColor = System.Drawing.SystemColors.InfoText;
+            this.ZTB.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZTB.ForeColor = System.Drawing.Color.DarkOrange;
+            this.ZTB.Location = new System.Drawing.Point(308, 86);
+            this.ZTB.Name = "ZTB";
+            this.ZTB.Size = new System.Drawing.Size(100, 25);
+            this.ZTB.TabIndex = 5;
+            // 
+            // YTB
+            // 
+            this.YTB.BackColor = System.Drawing.SystemColors.InfoText;
+            this.YTB.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YTB.ForeColor = System.Drawing.Color.DarkOrange;
+            this.YTB.Location = new System.Drawing.Point(174, 86);
+            this.YTB.Name = "YTB";
+            this.YTB.Size = new System.Drawing.Size(100, 25);
+            this.YTB.TabIndex = 6;
+            // 
+            // WTB
+            // 
+            this.WTB.BackColor = System.Drawing.SystemColors.InfoText;
+            this.WTB.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WTB.ForeColor = System.Drawing.Color.DarkOrange;
+            this.WTB.Location = new System.Drawing.Point(439, 86);
+            this.WTB.Name = "WTB";
+            this.WTB.Size = new System.Drawing.Size(100, 25);
+            this.WTB.TabIndex = 7;
+            // 
+            // InfoLB
+            // 
+            this.InfoLB.AutoSize = true;
+            this.InfoLB.BackColor = System.Drawing.Color.Indigo;
+            this.InfoLB.Font = new System.Drawing.Font("Segoe UI Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InfoLB.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.InfoLB.Location = new System.Drawing.Point(33, 9);
+            this.InfoLB.Name = "InfoLB";
+            this.InfoLB.Size = new System.Drawing.Size(465, 50);
+            this.InfoLB.TabIndex = 8;
+            this.InfoLB.Text = "Escribe en los campos de texto los valores. dependiendo\r\nlo que deseas calcular.";
+            // 
+            // RLB
+            // 
+            this.RLB.AutoSize = true;
+            this.RLB.BackColor = System.Drawing.Color.DodgerBlue;
+            this.RLB.Font = new System.Drawing.Font("Segoe UI Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RLB.Location = new System.Drawing.Point(244, 174);
+            this.RLB.Name = "RLB";
+            this.RLB.Size = new System.Drawing.Size(104, 30);
+            this.RLB.TabIndex = 9;
+            this.RLB.Text = "Resultado:";
+            this.RLB.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ResultadoLB
+            // 
+            this.ResultadoLB.AutoSize = true;
+            this.ResultadoLB.BackColor = System.Drawing.Color.Black;
+            this.ResultadoLB.Font = new System.Drawing.Font("Segoe UI Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultadoLB.ForeColor = System.Drawing.Color.Chartreuse;
+            this.ResultadoLB.Location = new System.Drawing.Point(219, 223);
+            this.ResultadoLB.Name = "ResultadoLB";
+            this.ResultadoLB.Size = new System.Drawing.Size(0, 32);
+            this.ResultadoLB.TabIndex = 10;
+            this.ResultadoLB.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label2.Location = new System.Drawing.Point(86, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label3.Location = new System.Drawing.Point(480, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "W";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label4.Location = new System.Drawing.Point(355, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Z";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label5.Location = new System.Drawing.Point(221, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Y";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(41, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "X: Precio";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(436, 174);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 34);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "X: Cateto opuesto\r\nY: Cateto adyacente\r\n";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(41, 274);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 68);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "X: Vel. final\r\nY: Vel. inicial\r\nZ: Tiempo\r\n(En SI)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label9.Location = new System.Drawing.Point(414, 274);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(164, 51);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "X: a, coeficiente cuadratico\r\nY: b, coeficiente lineal\r\nZ: c, constante\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(580, 352);
+            this.Controls.Add(this.ResultadoLB);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RLB);
+            this.Controls.Add(this.InfoLB);
+            this.Controls.Add(this.WTB);
+            this.Controls.Add(this.YTB);
+            this.Controls.Add(this.ZTB);
+            this.Controls.Add(this.XTB);
+            this.Controls.Add(this.HipotenusaButton);
+            this.Controls.Add(this.DistanciaButton);
+            this.Controls.Add(this.FCuadraticaButton);
+            this.Controls.Add(this.IVAButton);
+            this.Name = "Form1";
+            this.Text = "201318652_HT1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button IVAButton;
+        private System.Windows.Forms.Button FCuadraticaButton;
+        private System.Windows.Forms.Button DistanciaButton;
+        private System.Windows.Forms.Button HipotenusaButton;
+        private System.Windows.Forms.TextBox XTB;
+        private System.Windows.Forms.TextBox ZTB;
+        private System.Windows.Forms.TextBox YTB;
+        private System.Windows.Forms.TextBox WTB;
+        private System.Windows.Forms.Label InfoLB;
+        private System.Windows.Forms.Label RLB;
+        private System.Windows.Forms.Label ResultadoLB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ 201318652_HojaDeTrabajo_IPC2VD16/201318652_HojaDeTrabajo_IPC2VD16/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _201318652_HojaDeTrabajo_IPC2VD16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IVAButton_Click(object sender, EventArgs e)
+        {
+            double precio = Convert.ToDouble(XTB.Text);
+            if (XTB.Text != null)
+            {
+                double precioCosto = precio / 1.12;
+                double iva = precioCosto * 0.12;
+
+                ResultadoLB.Text = "Precio costo: " + Math.Round(precioCosto, 2).ToString() + "\n" + "IVA: " + Math.Round(iva).ToString();
+            } else
+            {
+                ResultadoLB.Text = "Ingresa un valor primero.";
+            }
+            
+        }
+    }
+}
